93e6c33f98a47860deca24fd48405a37b33a967c
guochunming <guochunming@vtstar.net>
2026-08-12 17:06:33 +0800

    防止label保存目录被open_dir_dialog冲掉，去掉二次加载导致第一张图片标注列表重复

--- labelImg.py
+++ labelImg.py
@@ -1379,9 +1379,7 @@ class MainWindow(QMainWindow, WindowMixin):
             target_dir_path = ustr(default_open_dir_path)
         self.last_open_dir = target_dir_path
         self.import_dir_images(target_dir_path)
-        self.default_save_dir = target_dir_path
-        if self.file_path:
-            self.show_bounding_box_from_annotation_file(file_path=self.file_path)
+        
 
     def import_dir_images(self, dir_path):
         if not self.may_continue() or not dir_path:
